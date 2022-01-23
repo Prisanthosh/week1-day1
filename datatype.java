a7947807bbcc5300afb730bda5799cfd7a033912
Prisanthosh <98250820+Prisanthosh@users.noreply.github.com>
2022-01-24 00:38:48 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,16 @@
+package learnselenium;
+public class datatype {
+public static void main(String[] args) {
+		short seatingCapacity = 125;
+		System.out.println("seating Capacity: "+seatingCapacity);
+		String carName="Waon";
+		System.out.println("car Name: "+carName);
+		int insurance = 10;
+		double number1 = 111.667888;
+		float number2 = 11.5F;
+        char alphabet = 'b';
+        long number3=123456789L;
+		boolean bool = false;
+		
+}
+}
